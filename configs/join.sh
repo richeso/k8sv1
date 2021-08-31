a7a2a6a2b5c497420e8a537a7f2ab8aa3160e19a
richard so <richard.cheso@gmail.com>
2021-08-31 13:00:32 -0400

    Updated

--- configs/join.sh
+++ configs/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token diq57s.w3505eqxsxy5faqj     --discovery-token-ca-cert-hash sha256:c0dac13accc992d3bb0126ac8df6bb52f2e40a95b17d2a4f6170e5a00c24030d 
+kubeadm join 192.168.56.10:6443 --token cr2bfk.6zut152c6nv4xtc2     --discovery-token-ca-cert-hash sha256:92f3e61b93450b77c29b5bf7a524f0baf4e73c7dd4f0aade780f07228852316c 
